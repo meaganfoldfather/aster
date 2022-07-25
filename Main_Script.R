70b1e0710a651e29d2e56036e9e14aeaddf70d24
MeaganOldfather <meagan.f.oldfather@gmail.com>
2022-07-25 13:28:59 -0600

    Created main script

---
+++ Main_Script.R
@@ -0,0 +1,2 @@
+# Build aster models to test for fitness effects of dispersal distance and maternal performance for the three focal Lasthenia species. 
+# Will need to bring in fitness data for both experimental individuals and maternal plants for all three years of the experiment 
