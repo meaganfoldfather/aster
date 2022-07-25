81d575d24070052f701f3c0c74872e0a1c3a73f3
MeaganOldfather <meagan.f.oldfather@gmail.com>
2022-07-25 16:29:41 -0600

    Rearranging dataframes for required aster model structure

--- Main_Script.R
+++ Main_Script.R
@@ -7,35 +7,34 @@ library(tidyverse)
 
 # Bring in 2018 experimental & maternal data #
 # experimental
-data_2018 <- read.csv("data/Jepson_2018_Traits.csv")
+data_2018 <- read_csv("data/Jepson_2018_Traits.csv")
 data_2018$year <- 2018
 
 #maternal
 maternal_2018 <- read_csv("data/Jepson_2017_Maternal.csv")
 maternal_2018$year <- 2018
-###########################################################
 
+###########################################################
 # Bring in 2019 experimental data 
 #experimental
-data_2019 <- read.csv("data/Jepson_2019_Traits.csv")
+data_2019 <- read_csv("data/Jepson_2019_Traits.csv")
 data_2019$year <- 2019
 
 #maternal
 maternal_2019 <- read_csv("data/JP_2018_Maternal.csv")
 maternal_2019$year <- 2019
-###########################################################
 
+###########################################################
 # Bring in 2022 experimental data 
 #experimental
-data_2022 <- source()
-data_2022$year <- 2022
+source("scripts/Combining_2022SurveySample.R") # creates prelim data_2022 dataframe
 
 #maternal 
 maternal_2022 <- read_csv("data/JP_2020_Maternal.csv")
 maternal_2022$year <- 2022
-###########################################################
 
-# Change dataframes so they talk nicely to each other any are prepared for required aster model structure
+###########################################################
+# Modifydataframes so they talk nicely to each other
 data_2018 <- data_2018[ ,c("group","mayID", "sp", "trans", "dist", "trt","loc","year", "status", "vs")]
 colnames(data_2018) <-c("group","id", "sp", "tran", "dist", "trt","loc","year", "status", "vs")
 data_2018 <-
@@ -53,12 +52,44 @@ data_2019 <-
     unite(col = "indiv",remove = FALSE, group, sp, id, tran, dist, trt, loc, year)
 data_2019
 
-# trait dataframe for 2022 only includes processed samples not all location with seeds --> need to find dataframe with status column to get an accurate estimate on how many individuals didnt germinate
 data_2022 <- data_2022[ ,c("group","id", "sp", "tran", "dist", "trt","loc","year", "status", "vs")]
-data_2019 <-
-    data_2019 %>% 
+data_2022 <-
+    data_2022 %>% 
     filter(!is.na(status)) %>% 
     unite(col = "mom", remove = FALSE, sp, group, id) %>% 
     unite(col = "indiv",remove = FALSE, group, sp, id, tran, dist, trt, loc, year)
-data_2019
+data_2022
 
+# combine all data
+all_data <- rbind(data_2018, data_2019, data_2022)
+all_data
+
+
+#####################################################################
+# make vs an integer
+all_data$vs <- as.integer(all_data$vs)
+
+# reshape for aster - need it to be a long-form where there is a row for each event - for survival and for the number of seeds 
+re_data<-
+ all_data %>%
+ pivot_longer(11:12, names_to = "vars", values_to = "resp") %>% 
+  arrange(vars)
+
+# change vars (status, vs) to a factor
+re_data$vars <- as.factor(re_data$vars)
+re_data
+
+# add needed columns/vectors
+re_data$root <- 1
+pred <- c(0, 1)
+fam <- c(1, 2); sapply(fam.default(), as.character)[fam]
+
+# make a fitness vector
+re_data$fit <- as.numeric(re_data$vars == "vs")
+
+#Make a numeric 'id' row
+nindvs_2019 <- 1:nrow(unique(re_all_field_data[re_all_field_data$year == 2019, "indiv"]))
+nindvs_2018 <-1:nrow(unique(re_all_field_data[re_all_field_data$year == 2018, "indiv"]))
+nindvs_2018 <- max(nindvs_2019) + nindvs_2018
+re_all_field_data$id <- c(nindvs_2019 , nindvs_2018 , nindvs_2019, nindvs_2018)
+re_all_field_data$id  <- as.integer(re_all_field_data$id)
--- scripts/Combining_2022SurveySample.R
+++ scripts/Combining_2022SurveySample.R
@@ -1,2 +1,29 @@
 #Combine 2022 survey and sample data
 
+# pull in sample data
+sample_2022 <- read_csv("data/Jepson_2022_Samples.csv")
+sample_2022
+
+# pull in survey data 
+survey_2022 <- read_csv("data/Jepson_2022_Survey.csv")
+survey_2022 <-
+survey_2022 %>% 
+  pivot_longer(cols = L1:L4, names_to = "locs")
+
+# mark status as 1 if germinated (B,F,V,S) and NA if missing (M,G)
+survey_2022[survey_2022$value == 0, "status"] <- 0
+survey_2022[survey_2022$value %in% c("V","B","F","f"), "status"] <- 1
+survey_2022[survey_2022$value %in% c("M","G"), "status"] <- NA
+survey_2022[survey_2022$sp == "glab", "sp"] <- "gla"
+unique(survey_2022$status)
+
+#combine survey and sample
+colnames(survey_2022) <- c("group", "sp", "id", "mtag", "tran", "dist","trt","tag","side","Notes","loc", "value","status")
+data_2022 <- left_join(survey_2022, sample_2022, by = c("group", "sp","id","tran","dist","trt","loc"))
+
+# add year columns
+data_2022$year <- 2022
+
+data_2022
+
+
